e1253cf460723053806a02ff4360fb7d4ed0a033
xaesu <xaesu6@gmail.com>
2024-10-27 22:53:59 +0900

    Modify planet scripts

--- Solar-System/Assets/Scripts/PlanetRevolve.cs
+++ Solar-System/Assets/Scripts/PlanetRevolve.cs
@@ -7,8 +7,13 @@ public class PlanetRevolve : MonoBehaviour
     [SerializeField]
     GameObject fixedStar;
     [SerializeField]
-    float revolutionSpeed = 10f;    // 공전속도
-
+    float revolutionSpeed = 0.5f;        // 공전속도
+    [SerializeField]
+    float majorAxisRound = 10.0f;       // 장축 반지름 (x축)
+    [SerializeField]
+    float minorAxisRound = 9.0f;        // 단축 반지름 (z축)
+    float angle = 0.0f;                 // 현재 각도
+    
     void Start()
     {
         // 중심 오브젝트 (항성) 지정 -> 태양계 : 태양
@@ -17,7 +22,21 @@ public class PlanetRevolve : MonoBehaviour
 
     void Update()
     {
-        // 중심 오브젝트 기준 회전 (원 궤도)
-        transform.RotateAround(fixedStar.transform.position, Vector3.up, revolutionSpeed * Time.deltaTime);
+        //// 중심 오브젝트 기준 회전 (원 궤도)
+        //transform.RotateAround(fixedStar.transform.position, Vector3.up, revolutionSpeed * Time.deltaTime);
+
+        // 각도 변환
+        angle += revolutionSpeed * Time.deltaTime;
+
+        // x, z좌표 타원 방정식 계산
+        float x = majorAxisRound * Mathf.Cos(angle);
+        float z = minorAxisRound * Mathf.Sin(angle);
+
+        // 타원 궤도 위치로 이동
+        transform.position = new Vector3(x, 0, z) + fixedStar.transform.position;
+
+        // 오일러 각도 회전으로 타원 궤도 설정
+        Vector3 currnetDirection = fixedStar.transform.position - transform.position;       // 항성 중심 회전
+        transform.eulerAngles = new Vector3(0, Mathf.Atan2(currnetDirection.x, currnetDirection.z) * Mathf.Rad2Deg, 0);
     }
 }
--- Solar-System/Assets/Scripts/PlanetRotation.cs
+++ Solar-System/Assets/Scripts/PlanetRotation.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class PlanetRotation : MonoBehaviour
 {
     [SerializeField]
-    float rotationSpeed = 15f;    // 자전속도
+    float rotationSpeed = 15f;      // 자전속도
+    [SerializeField]
+    float rotationAxis = -23.0f;    // 자전축
+
+    void Awake()
+    {
+        transform.eulerAngles = new Vector3(0, 0, rotationAxis);
+    }
 
     void Update()
     {
-        // 자전 구현 (반시계 방향 회전 기준)
+        // 자전 구현 (반시계 방향 회전 기준) 
         transform.Rotate(Vector3.up * -rotationSpeed * Time.deltaTime);
     }
 }
