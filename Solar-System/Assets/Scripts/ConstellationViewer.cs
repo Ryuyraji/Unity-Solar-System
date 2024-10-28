68caec46b5f35caa6272778f7d248ebb81959d8e
Ryuyraji <yeaji1839@gmail.com>
2024-10-29 01:09:49 +0900

    csv
    csv파일 데이터 기본 클래스 생성했습니다

---
+++ Solar-System/Assets/Scripts/ConstellationViewer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstellationViewer : MonoBehaviour
+{
+
+    [SerializeField]
+    TextAsset starDataCSV;
+    [SerializeField]
+    TextAsset starMajorDataCSV;     
+    [SerializeField]
+    TextAsset constellationNameDataCSV;
+    [SerializeField]
+    TextAsset constellationPositionDataCSV;
+    [SerializeField]
+    TextAsset constellationLineDataCSV;
+}
